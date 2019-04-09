8b716213e5bfb8b32e34a20de39e760fec15b4ed
陈前坤 <48710163+chen5864647@users.noreply.github.com>
2019-04-10 00:43:52 +0800

    Update normalaccount.h

--- Bank/base/Account/normalaccount.h
+++ Bank/base/Account/normalaccount.h
@@ -5,9 +5,17 @@
 #ifndef BANK_NORMALACCOUNT_H
 #define BANK_NORMALACCOUNT_H
 
+#include <list>
+
 #include "account.h"
 
+class NormalAccount;
+
+void saver(const NormalAccount &normalacc);
+
 class NormalAccount : public Account {
+    friend void saver(const NormalAccount &normalacc);
+
 public:
     NormalAccount(std::string userName, double money, unsigned int startYear, std::string password = "888888");
 
@@ -16,6 +24,12 @@ public:
     ~NormalAccount() override ;
 
     void setPassword(std::string newPassword);
+
+    std::string getName() const override ;
+
+    static std::list<std::string> NormalUserNames;
+
+    static bool judgeExist(std::string tempUserName);
 
 private:
     bool _setPassword(std::string newPassword);
